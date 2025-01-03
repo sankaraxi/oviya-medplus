a733ba76c92aa074e25a3e800dbfc285fb35af89
sankaraxi <sankarfrompalani@gmail.com>
2025-01-03 17:17:18 +0530

    refactor StrategySection component to dynamically render services; enhance layout and styling for better presentation

--- src/components/services/StrategySection.jsx
+++ src/components/services/StrategySection.jsx
@@ -4,15 +4,24 @@ console.log(strategyData)
 
 const StrategySection = () => {
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center pt-5 pb-10'>
         <h1 className='font-oswald text-center font-semibold text-heading text-[28px]'>Oviya MedSafe has formed strong strategic alliances where we can provide
         <br/>the following services:</h1>
-        <div className='w-[280px] h-[283px] bg-white shadow-xl flex flex-col items-center justify-center mt-10'>
-            <div>
-                <img src={strategyData[0]?.image_id} alt="service" className='' />
-            </div>
-            <p className='pt-7 font-bold'>{strategyData[0].title}</p>
+        <div className='w-9/12 flex flex-wrap justify-center items-center mt-10'>
+            {
+                strategyData.map((item) => (
+                    <div key={item.id} className='w-[280px] h-[283px] rounded-lg mr-16 bg-white shadow-lg flex flex-col items-center justify-center mt-10'>
+                        <div>
+                            <img src={item?.image_id} alt="service" className='' />
+                        </div>
+                        <p className='pt-7 font-dmsans font-bold'>{item.title}</p>
+                    </div>
+                ) )
+            }
+
         </div>
+        
+        
     </div>
   )
 }
